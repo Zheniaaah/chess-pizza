d5407dfc2f72daceb85aafbc52e5f2c5f687d802
Zheniaaah <124299452+Zheniaaah@users.noreply.github.com>
2025-12-24 15:27:39 +0200

    feat(api): created search request

---
+++ app/api/products/search/route.ts
@@ -0,0 +1,19 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+import { prisma } from '@/lib';
+
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams.get('query') || '';
+
+  const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: query,
+        mode: 'insensitive',
+      },
+    },
+    take: 5,
+  });
+
+  return NextResponse.json(products);
+}
